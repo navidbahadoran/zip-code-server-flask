bd85e55d8d688dae311b97cae814d110587eb055
Navid Bahadoran <navid.bahadoran@gmail.com>
2019-07-18 22:37:56 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import os
+import csv
+
+app = Flask(__name__)
+
+
+def find_coordinate(zip_code):
+    header = ['ZIP', 'LAT', 'LNG']
+    with open('zipcode.txt') as csv_file:
+        csv_reader = csv.DictReader(csv_file, fieldnames=header)
+        for item in csv_reader:
+            if item['ZIP'] == str(zip_code):
+                return f'Lat:{item["LAT"]}, Long:{item["LNG"]}'
+    return 'The zipcode is not in the list'
+
+
+@app.route('/<zip_code>/')
+def get_coordinate(zip_code):
+    return find_coordinate(zip_code)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 6738))
+    app.run(host='127.0.0.1', port=port, debug=True)
